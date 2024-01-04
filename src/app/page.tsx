254fcdc4b215ec424a4ea904894b1c0804174c2a
GonAvi <dgaduff@gmail.com>
2024-01-03 23:46:29 -0600

    Lil style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,26 @@ const loadItems = async () => {
 }
 
 const Home = async () => {
-  const items: Item[] = await loadItems()
   return (
-    <div className='flex group justify-center items-center w-screen h-dvh overflow-hidden'>
-      <Item name='David' imageUrl='https://phantom-marca-mx.unidadeditorial.es/6c3713a75c19a61f263ca12c4cfeff20/resize/1200/f/jpg/mx/assets/multimedia/imagenes/2023/06/05/16859985858899.jpg' popularity={20} />
-      <Item name='Alan' imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Bicho_G%C3%B3mez_en_la_Feria_de_Teatro_x_la_Identidad_en_Tecn%C3%B3polis_%28cropped%29.jpg/640px-Bicho_G%C3%B3mez_en_la_Feria_de_Teatro_x_la_Identidad_en_Tecn%C3%B3polis_%28cropped%29.jpg' popularity={20} />
-    </div>
+    <>
+      <div className='group flex flex-col md:flex-row relative justify-center items-center w-screen h-dvh overflow-hidden'>
+        <Item
+          name='David'
+          imageUrl='https://phantom-marca-mx.unidadeditorial.es/6c3713a75c19a61f263ca12c4cfeff20/resize/1200/f/jpg/mx/assets/multimedia/imagenes/2023/06/05/16859985858899.jpg'
+          popularity={20}
+          position='start'
+        />
+        <div className='flex-auto z-10 max-h-0 flex items-center justify-center max-w-0'>
+          <div className='group-has-[.start:hover]:-rotate-45 group-has-[.end:hover]:rotate-45 transition-all duration-300 ease-in-out md:p-4 md:text-6xl p-2 text-4xl bg-slate-700 rounded-xl z-10'>OR</div>
+        </div>
+        <Item
+          name='Alan'
+          imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Bicho_G%C3%B3mez_en_la_Feria_de_Teatro_x_la_Identidad_en_Tecn%C3%B3polis_%28cropped%29.jpg/640px-Bicho_G%C3%B3mez_en_la_Feria_de_Teatro_x_la_Identidad_en_Tecn%C3%B3polis_%28cropped%29.jpg'
+          popularity={20}
+          position='end'
+        />
+      </div>
+    </>
   )
 }
 
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Item = ({ name, popularity, imageUrl }: { name: string, popularity: number, imageUrl: string }) => {
+const Item = ({ name, popularity, imageUrl, position = 'start' }: { name: string, popularity: number, imageUrl: string, position?: 'end' | 'start' }) => {
     return (
-        <div  className='hover:bg-rose-500 w-1/2 group-has-[:hover]:bg-red-700 has-[:hover]:w-3/4 bg-cover bg-center flex-auto bg-rose-500 h-full transition-all duration-500 ease-in-out flex justify-center items-center'>
-            <div className='w-4/6 h-full flex justify-center items-center bg-slate-700'>
+        <div style={{ backgroundImage: `url(${imageUrl})` }} className={` md:w-1/2 md:has-[.attention:hover]:w-3/4 md:has-[attention:hover]:h-full md:h-full w-full h-1/2 has-[attention:hover]:h-3/4 bg-cover relative bg-center flex-auto bg-rose-500 transition-all duration-500 ease-in-out flex justify-center items-center`}>
+            <div className={`${position} attention peer ${position === 'start' ? `left-0 top-0` : `right-0 bottom-0`} md:w-3/4 md:h-full w-full h-3/4 absolute z-20`} ></div>
+            <div className='w-full h-full peer-hover:backdrop-blur-0 peer-hover:bg-transparent transition-all duration-500 bg-black/40 backdrop-blur-sm absolute ' />
+            <div className='z-10 flex justify-center items-center'>
                 <h4>{name}</h4>
             </div>
         </div>
